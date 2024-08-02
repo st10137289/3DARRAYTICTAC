683bd607c378f4340b79103936640e122f3ab3b3
st10137289 <165705727+st10137289@users.noreply.github.com>
2024-08-02 14:51:16 +0300

    Changes made so it works in netbeans

--- 3D Array Column Addition/src/Main.java
+++ 3D Array Column Addition/src/Main.java
@@ -27,17 +27,17 @@ public class Main
         
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                System.out.print(ticTac[i][j] + "\t"); 
+                System.out.print(ticTac[i][j] + "    "); 
             }
-            System.out.println("= " + rowSums[i]);  
+            System.out.println("=   " + rowSums[i]);  
         }
 
         
-        System.out.println("=   =   =");
+        System.out.println("=    =    =");
 
         
         for (int j = 0; j < 3; j++) {
-            System.out.print(colSums[j] + "\t");  
+            System.out.print(colSums[j] + "   ");  
         }
     }
 }
